9c8002d2a852733b988a8eada4a260e0b5df4416
Kartikey45 <singhkartikey45@gmail.com>
2020-05-13 13:38:34 +0530

    ADDED : Created API for Sample App

---
+++ SampleCL/ClassCL.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SampleCL
+{
+    public class ClassCL
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ SampleRL/Services/SSampleRL.cs
@@ -0,0 +1,15 @@
+﻿using SampleCL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleCL.Services
+{
+    public class SSampleRL : ISampleRL
+    {
+        public string Return_Name(Sample model)
+        {
+            return "My name is " + model.Name;
+        }
+    }
+}
